64876ae774e88abf5c074d1778d81b8204b89265
Tom Potter <tom.potter@bbc.co.uk>
2026-04-23 16:51:29 +0100

    Upgrade embedding model

--- chroma/server_example.py
+++ chroma/server_example.py
@@ -1,10 +1,30 @@
 import json
+from uuid import uuid4
 
 import chromadb
 import polars
 from utils import HuggingFaceEmbeddingModel
 
 DATA_CSV_FILE = "../bbc_news_transcripts.csv"
+
+
+def preprocess_dataframe(dataframe):
+
+    new_dataframe = (
+        dataframe
+        # 1) Add a unique ID per original row
+        .with_row_index("row_id")
+        # 2) Split the target column on the substring delimiter
+        .with_columns(polars.col("body").str.split("NEW STORY").alias("new_story"))
+        # 3) Turn list of chunks into multiple rows (duplicating other columns)
+        .explode("new_story")
+        # 4) Clean + remove empties
+        .with_columns(polars.col("new_story").str.strip_chars()).filter(
+            polars.col("new_story").is_not_null() & (polars.col("new_story") != "")
+        )
+    )
+
+    return new_dataframe
 
 
 if __name__ == "__main__":
@@ -19,30 +39,34 @@ if __name__ == "__main__":
 
     # Load transcript data from local CSV file
     transcripts = polars.read_csv(DATA_CSV_FILE)
+    transcripts = preprocess_dataframe(transcripts)
     print(transcripts.head())
 
     # Embedding model
-    emb_model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    emb_model_name = "microsoft/harrier-oss-v1-270m"
+    # emb_model_name = "sentence-transformers/all-MiniLM-L6-v2"
+
     embedding_model = HuggingFaceEmbeddingModel(
         model_name=emb_model_name,
     )
 
-    subset_size = 10
+    subset_size = 100
 
     if collection.count() < subset_size:
         # Generate embeddings
         transcripts_subset = transcripts.sample(subset_size)
-        documents = transcripts_subset["body"].to_list()
-
+        documents = transcripts_subset["new_story"].to_list()
         metadata = [
-            {"subject": subject} for subject in transcripts_subset["subject"].to_list()
+            {"subject": str(row["subject"]), "timestamp": str(row["date"])}
+            for row in transcripts_subset.iter_rows(named=True)
         ]
 
         embeddings = embedding_model(documents)
+        print(f"Embeddings: ({len(embeddings)}, {len(embeddings[0])})")
 
         # Add new embeddings to vector database
         collection.add(
-            ids=[f"id-{i}" for i in range(subset_size)],
+            ids=[str(uuid4()) for _ in range(subset_size)],
             embeddings=embeddings,
             documents=documents,
             metadatas=metadata,  # type: ignore
@@ -52,8 +76,8 @@ if __name__ == "__main__":
     print(f"\nTotal elements in vector database: {total_elements}", end="\n\n")
 
     # Query vector database
-    query = "Prime Minister's response to Peter Mandleson"
-    n_results = 1
+    query = "Update on the war in the Middle East"
+    n_results = 3
 
     query_embedding = embedding_model([query])
     result = collection.query(query_embeddings=query_embedding, n_results=n_results)
--- chroma/utils.py
+++ chroma/utils.py
@@ -1,8 +1,9 @@
 import os
 
 import numpy as np
-import requests
 from chromadb.api.types import EmbeddingFunction
+from huggingface_hub import InferenceClient
+from tqdm import tqdm
 
 
 class HuggingFaceEmbeddingModel(EmbeddingFunction):
@@ -12,30 +13,24 @@ class HuggingFaceEmbeddingModel(EmbeddingFunction):
         self.model_name = model_name
         self.api_key = self.get_hf_token()
 
-        self.url = (
-            f"https://router.huggingface.co/hf-inference/models/"
-            f"{model_name}/pipeline/feature-extraction"
+        self.client = InferenceClient(
+            provider="hf-inference",
+            api_key=self.api_key,
         )
-
-        self.headers = {
-            "Authorization": f"Bearer {self.api_key}",
-            "Content-Type": "application/json",
-        }
 
     @staticmethod
     def get_hf_token():
         return os.getenv("HF_TOKEN")
 
     def __call__(self, texts: list[str]) -> list[np.array]:  # type: ignore
-        payload = {"inputs": texts}
+        # Run inference on embedding model in HF Hub
+        data_array = []
 
-        r = requests.post(self.url, headers=self.headers, json=payload)
-
-        if r.status_code != 200:
-            raise RuntimeError(f"HuggingFace API error {r.status_code}: {r.text}")
-
-        # Return embeddings data as float32 numpy arrays (what Chroma expects)
-        data_json = r.json()
-        data_array = [np.array(vec, dtype=np.float32) for vec in data_json]
+        for text in tqdm(texts, desc="Generating embeddings"):
+            embedding = self.client.feature_extraction(
+                text,
+                model=self.model_name,
+            )
+            data_array.append(embedding)
 
         return data_array
